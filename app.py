f218109e609517e78c03cc7b7fdf195cc76fe8b3
vasudha Donthamsetti <donthamsettivasudha@gmail.com>
2025-04-12 11:06:04 +0530

    updated

--- app.py
+++ app.py
@@ -303,4 +303,4 @@ async def websocket_endpoint(websocket: WebSocket, city: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="127.0.0.1", port=8000)
+    uvicorn.run("app:app", host="127.0.0.1", port=8001)
